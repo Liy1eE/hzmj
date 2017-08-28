f75fed8cc0014a85663641531975f014cc25a445
liuyang <stb@qq.com>
2017-08-28 11:51:29 +0800

    init

---
+++ hzmj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hzmj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
